5e0421ee656b63af61c7001599141777e4945b7f
inkst4r <inkst4r@proton.me>
2026-05-07 09:52:58 -0400

    e

--- Assets/Scripts/startGame.cs
+++ Assets/Scripts/startGame.cs
@@ -7,6 +7,7 @@ public class startGame : MonoBehaviour
 {
     public GameObject startButton;
     public GameObject startScreen;
+    public GameObject quitButton;
     public GameObject fadeOutScreen;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
